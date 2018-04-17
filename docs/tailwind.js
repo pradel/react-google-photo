fb77f668fba6590232bc4685d7e3aa5979e09f76
Pradel Léo <pradel.leo@gmail.com>
2018-04-17 23:35:17 +0200

    try to reduce tailwindcss size

--- docs/tailwind.js
+++ docs/tailwind.js
@@ -1,9 +1,34 @@
-let defaultConfig = require('tailwindcss/defaultConfig')();
+const _ = require('lodash');
+const defaultConfig = require('tailwindcss/defaultConfig')();
 
-module.exports = {
+const colors = _.pick(defaultConfig.colors, [
+  'black',
+  'grey-darkest',
+  'grey-darker',
+  'grey-dark',
+  'grey',
+  'grey-light',
+  'grey-lighter',
+  'grey-lightest',
+  'white',
+  'teal-darkest',
+  'teal-darker',
+  'teal-dark',
+  'teal',
+  'teal-light',
+  'teal-lighter',
+  'teal-lightest',
+]);
+
+const config = {
   ...defaultConfig,
-  screens: {
-    ...defaultConfig.screens,
-    xl: '992px',
-  },
+  colors: colors,
+  textColors: colors,
+  backgroundColors: colors,
+  borderColors: global.Object.assign({ default: colors['grey-light'] }, colors),
+  screens: _.pick(defaultConfig.screens, ['sm', 'md', 'lg']),
 };
+
+// console.log(config);
+
+module.exports = config;
