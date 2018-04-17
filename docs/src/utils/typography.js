b8b7f201bea8aa89d699a0cbc75d8ffd97ee4ad2
Pradel Léo <pradel.leo@gmail.com>
2018-04-17 22:04:00 +0200

    change typo

--- docs/src/utils/typography.js
+++ docs/src/utils/typography.js
@@ -1,6 +1,15 @@
 import Typography from 'typography';
 import sutroTheme from 'typography-theme-sutro';
 
+sutroTheme.googleFonts[0] = { ...sutroTheme.googleFonts[1], name: 'Rubik' };
+sutroTheme.headerFontFamily[0] = 'Rubik';
+
+sutroTheme.googleFonts[1] = {
+  styles: ['400', '400i', '700', '700i'],
+  name: 'Lato',
+};
+sutroTheme.bodyFontFamily[0] = 'Lato';
+
 const typography = new Typography(sutroTheme);
 
 export default typography;
